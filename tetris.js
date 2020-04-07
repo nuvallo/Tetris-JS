8dc79fc82e23f2f75979b479ab388547ea55418d
nuvallo <nuvallo.inc@gmail.com>
2020-04-07 12:34:00 -0400

    rotating matrix complete

--- tetris.js
+++ tetris.js
@@ -41,6 +41,7 @@ function drawMatrix(matrix, offset) {
   });
 }
 
+// Allows stacking of matrix pieces
 function collide(arena, player) {
   const [m, o] = [player.matrix, player.pos];
   for (let y = 0; y < m.length; ++y) {
@@ -73,16 +74,47 @@ function playerDrop() {
   dropCounter = 0;
 }
 
+// Sets barriers around arena
 function playerMove(dir) {
-  player.pos.x + dir;
+  player.pos.x += dir;
   if (collide(arena, player)) {
-    player.pos.x + dir;
+    player.pos.x -= dir;
   }
 }
 
 // drop animation
 let dropCounter = 0;
 let dropInterval = 1000;
+
+function playerRotate(dir) {
+  const pos = player.pos.x;
+  let offset = 1;
+  rotate(player.matrix, dir);
+  while (collide(arena, player)) {
+    player.pos.x += offset;
+    offset = offset + (offset > 0 ? 1 : -1);
+
+    if (offset > player.matrix[0].length) {
+      rotate(player.matrix, -dir);
+      player.pos.x = pos;
+      return;
+    }
+  }
+}
+
+// Rotating matrix
+function rotate(matrix, dir) {
+  for (let y = 0; y < matrix.length; ++y) {
+    for (let x = 0; x < y; ++x) {
+      [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
+    }
+  }
+  if (dir > 0) {
+    matrix.forEach((row) => row.reverse());
+  } else {
+    matrix.reverse();
+  }
+}
 
 let lastTime = 0;
 function update(time = 0) {
@@ -114,7 +146,10 @@ document.addEventListener("keydown", (event) => {
     playerMove(1);
   } else if (event.keyCode === 40) {
     playerDrop();
+  } else if (event.keyCode === 81) {
+    playerRotate(-1);
+  } else if (event.keyCode === 87) {
+    playerRotate(1);
   }
 });
-
 update();
