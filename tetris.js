b694f655860407335491e4953b34929081207a7b
nuvallo <nuvallo.inc@gmail.com>
2020-04-07 13:30:55 -0400

    Cleared when pieces hit the top border

--- tetris.js
+++ tetris.js
@@ -23,7 +23,6 @@ function createPiece(type) {
     return [
       [1, 1],
       [1, 1],
-      [1, 1],
     ];
   } else if (type === "L") {
     return [
@@ -109,7 +108,7 @@ function playerDrop() {
   if (collide(arena, player)) {
     player.pos.y--;
     merge(arena, player);
-    player.pos.y = 0;
+    playerReset();
   }
   dropCounter = 0;
 }
@@ -119,6 +118,17 @@ function playerMove(dir) {
   player.pos.x += dir;
   if (collide(arena, player)) {
     player.pos.x -= dir;
+  }
+}
+
+function playerReset() {
+  const pieces = "ILJOTSZ";
+  player.matrix = createPiece(pieces[(pieces.length * Math.random()) | 0]);
+  player.pos.y = 0;
+  player.pos.x =
+    ((arena[0].length / 2) | 0) - ((player.matrix[0].length / 2) | 0);
+  if (collide(arena, player)) {
+    arena.forEach((row) => row.fill(0));
   }
 }
 
