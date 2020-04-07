a3ba9e944b6425aa1ccfc4d4d70bd7717c91bc62
nuvallo <nuvallo.inc@gmail.com>
2020-04-07 12:07:57 -0400

    storing the matrix at the bottom

--- tetris.js
+++ tetris.js
@@ -10,10 +10,21 @@ const matrix = [
   [0, 1, 0],
 ];
 
+function createMatrix(w, h) {
+  const matrix = [];
+  while (h--) {
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
+}
+
 function draw() {
   // Canvas
   context.fillStyle = "#000";
   context.fillRect(0, 0, canvas.clientWidth, canvas.height);
+
+  drawMatrix(arena, { x: 0, y: 0 });
+
   drawMatrix(player.matrix, player.pos);
 }
 
@@ -30,9 +41,43 @@ function drawMatrix(matrix, offset) {
   });
 }
 
+function collide(arena, player) {
+  const [m, o] = [player.matrix, player.pos];
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x) {
+      if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+}
+
 function playerDrop() {
   player.pos.y++;
+  if (collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
   dropCounter = 0;
+}
+
+function playerMove(dir) {
+  player.pos.x + dir;
+  if (collide(arena, player)) {
+    player.pos.x + dir;
+  }
 }
 
 // drop animation
@@ -52,6 +97,9 @@ function update(time = 0) {
   requestAnimationFrame(update);
 }
 
+// where pieces collect
+const arena = createMatrix(12, 20);
+
 // player containing position and metrix
 const player = {
   pos: { x: 1, y: 5 },
@@ -61,9 +109,9 @@ const player = {
 // Event to move positioning
 document.addEventListener("keydown", (event) => {
   if (event.keyCode === 37) {
-    player.pos.x--;
+    playerMove(-1);
   } else if (event.keyCode === 39) {
-    player.pos.x++;
+    playerMove(1);
   } else if (event.keyCode === 40) {
     playerDrop();
   }
